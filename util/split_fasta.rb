21a0c797cec4cf319bf85ac264d40a1a30430bda
Shigenobu Shuji <shige@catm.nibb.ac.jp>
2012-11-09 21:00:16 +0900

    target dir should be specified.

--- util/split_fasta.rb
+++ util/split_fasta.rb
@@ -4,6 +4,24 @@ include Bio
 
 $fastaf = ARGV[0]
 $num_entries = ARGV[1].to_i
+$outdir = ARGV[2]
+
+def print_help
+  puts "Usage:"
+  puts "  #{File.basename($0)}  fasta_file  num_entries_per_file  [target_dir]"
+  puts ""
+  exit
+end
+
+unless ARGV.size == 3
+  print_help
+end
+
+if File.exists?($outdir)
+  raise "Directory, #{$outdir}, already exists."
+else
+  Dir.mkdir($outdir)
+end
 
 container = []
 i = 0
@@ -11,12 +29,14 @@ FlatFile.open(FastaFormat, ARGV[0]).each do |fas|
   container << fas
   if container.size == $num_entries
     outf = File.basename($fastaf) + "_#{i+=1}.fasta"
-    STDERR.puts [outf, container.size].join("\t")
-    File.open(outf, "w"){|o| o.puts container}
+    outpath = $outdir + "/" + outf
+    STDERR.puts [outpath, container.size].join("\t")
+    File.open(outpath, "w"){|o| o.puts container}
     container = []
   end
 end
 
 outf = File.basename($fastaf) + "_#{i+=1}.fasta"
-File.open(outf, "w"){|o| o.puts container}
-STDERR.puts [outf, container.size].join("\t")
+outpath = $outdir + "/" + outf
+File.open(outpath, "w"){|o| o.puts container}
+STDERR.puts [outpath, container.size].join("\t")
